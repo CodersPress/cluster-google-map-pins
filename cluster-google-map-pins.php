573c2bda219c06d74e3aba5df69c15f99e5f0434
Martin <support@coderspress.com>
2015-05-02 23:04:53 +0100

    updated

--- cluster-google-map-pins.php
+++ cluster-google-map-pins.php
@@ -11,7 +11,7 @@ License:
 */
 add_action( 'init', 'clp_plugin_updater' );
 function clp_plugin_updater() {
-	include_once 'updater.php';
+	include_once( dirname( __FILE__ ) . '/updater.php' );
 	define( 'WP_CLP_FORCE_UPDATE', true );
 	if ( is_admin() ) { 
 		$config = array(
